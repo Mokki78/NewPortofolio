2b817e3fc085e4f885b9c81e5dc9bdc3126d2306
Mokki78 <100276996+Mokki78@users.noreply.github.com>
2024-12-09 23:41:10 +0100

    Added hide and show effect

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
 
+var div = document.querySelector(".open-close")
+var display = 0;
+
+function  hideShow()
+{
+     if(display == 1) 
+    {
+    div.style.display = "block";
+    display = 0;
+     }
+    else
+    {
+     div.style.display = "none";
+     display = 1;
+
+    }
+}
+
+
