356870a8cd0deec94be07d2214f9a6b24922f7fb
Mokki78 <100276996+Mokki78@users.noreply.github.com>
2024-12-17 10:39:07 +0100

    Hamburger & overlay

---
+++ src/js/hamburger-menu.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("nav-mobile").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("nav-mobile").style.width = "0%";
+  }
+
+  openNav();
+
+  closeNav();
